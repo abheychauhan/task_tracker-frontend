003d6351ff1b0d09c4450b553183c7a23f35f5af
Abhey Singh <abheys172@gmail.com>
2025-04-29 23:41:39 +0530

    fix bugs

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://zucchini-liberation-production.up.railway.app//task-tracker',
+    baseURL: 'https://zucchini-liberation-production.up.railway.app/task-tracker',
 
 
 });
